9af9aada56060bda98319d76819890503c2d2cf9
davidalexteixeira <david5teixeira@gmail.com>
2018-01-26 11:02:25 +0100

    organization

--- battle.js
+++ battle.js
@@ -31,8 +31,8 @@ Battle.prototype.attack = function(command) { //must change to execCommand
     // case attack -> check turn and attack
 
     var hitChance = Math.floor(Math.random() * 100);
-    var snd = new Audio("./Sounds/woosh.mp3"); // buffers automatically when created
-    var snd1 = new Audio('./Sounds/punch.mp3')
+    var snd = new Audio("./Sounds/woosh.mp3");
+    var snd1 = new Audio('./Sounds/punch.mp3');
 
     
     if (self.currentTurn === 0 && hitChance > 50) { 
@@ -52,10 +52,18 @@ Battle.prototype.attack = function(command) { //must change to execCommand
 Battle.prototype.specialAttack = function () {
     var self = this; 
 
-    if (self.currentTurn === 0) {
-        self.playerTwo.receiveDamage(self.playerOne.specialAttack());   
-    } else {
+    var hitChance = Math.floor(Math.random() * 100)
+    var snd = new Audio("./Sounds/special-attack-miss.mp3");
+    var snd1 = new Audio('./Sounds/special-attack-success.mp3');
+
+    if (self.currentTurn === 0 && hitChance > 80) {
+        self.playerTwo.receiveDamage(self.playerOne.specialAttack());
+        snd1.play();   
+    } else if(self.currentTurn === 1 && hitChance > 80){
         self.playerOne.receiveDamage(self.playerTwo.specialAttack()); 
+        snd1.play();
+    } else {
+        snd.play();
     }
     self.checkIfDeath();
     console.log(self.playerOne.name, self.playerOne.health);
@@ -65,11 +73,14 @@ Battle.prototype.specialAttack = function () {
 
 Battle.prototype.defensiveBonus = function () {
     var self = this;
+    var snd = new Audio("./Sounds/defense.mp3")
 
     if (self.currentTurn === 0) {
-        self.playerOne.getBonus();   
+        self.playerOne.getBonus();
+        snd.play();   
     } else {
         self.playerTwo.getBonus(); 
+        snd.play();
     }
     self.checkIfDeath();
     console.log(self.playerOne.name, self.playerOne.health);
